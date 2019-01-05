68696c99f9d665c9371fb95276d74f87f9d4ab63
Tracie Masek <traciemasek@gmail.com>
2019-01-05 20:19:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
-    cart.slice(0);
+    cart.slice(0, cart.length +1);
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`
 
 }
